3f3806ff94fa62d1ae15629b50643203e25db958
rishithapaladugu <116055808+rishithapaladugu@users.noreply.github.com>
2025-10-01 14:11:24 +0530

    Create Navigation .js

---
+++ src/components/Navigation .js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+import React from "react";
+export default function Navigation() {
+    return (
+        <nav>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                  <li><Link to="/about">About</Link></li>
+            </ul>
+        </nav>
+    )
+}
